f9eed8eaf79441f624b2ff45f864b08f89b03b4d
Aditya Kumar <97836516+adityak54@users.noreply.github.com>
2024-05-29 21:27:59 +0530

    added restaurant search endpoint

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import "dotenv/config";
 import myUserRoute from "./routes/MyUserRoute"
 import myRestaurantRoute from "./routes/MyRestaurantRoute"
+import restaurantRoute from "./routes/RestaurantRoute"
 import mongoose from 'mongoose'
 import {v2 as cloudinary} from "cloudinary"
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -25,6 +26,7 @@ app.get("/health",async (req:Request, res:Response)=>{
 })
 app.use("/api/my/user",myUserRoute);
 app.use("/api/my/restaurant",myRestaurantRoute);
+app.use("/api/restaurant",restaurantRoute);
 app.listen(7000,()=>{
     console.log("Server started on port 7000");
 })
